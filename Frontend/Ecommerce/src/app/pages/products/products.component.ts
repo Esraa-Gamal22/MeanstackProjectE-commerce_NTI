0e9870a8cee53f44383c663ae960cc06f3229c33
esraa-gamal22 <esraagamal2688@gmail.com>
2022-07-02 23:37:01 +0200

    connect with api

--- Frontend/Ecommerce/src/app/pages/products/products.component.ts
+++ Frontend/Ecommerce/src/app/pages/products/products.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  recivedCatId: any ;
 
-  ngOnInit(): void {
-  }
+  //array before filteration
   productsArray = [
     
   ];
+  // array after filtiration
+  filterdArray:any = [];
 
+  constructor(
+   
+  ) {}
 
+  ngOnInit(): void {
+   
+    this.filterCatProducts();
+  }
+  
+  filterCatProducts() {
+    this.prdService
+      .getProductsByCatId(this.recivedCatId)
+      .subscribe((products) => {
+        this.filterdArray = products.data;
+      });
+  }
 }
